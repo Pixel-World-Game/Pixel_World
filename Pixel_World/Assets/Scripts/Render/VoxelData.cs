e3135f76c5d9ae2969f79ec2a663372f7e73ee00
Ruihan Zhao <ruihan.ryen.zhao@gmail.com>
2025-01-02 15:38:39 +0000

    chunk gen

--- Pixel_World/Assets/Scripts/Render/VoxelData.cs
+++ Pixel_World/Assets/Scripts/Render/VoxelData.cs
@@ -6,16 +6,18 @@ public static class VoxelData {
 
 	public static readonly int ChunkWidth = 5;
 	public static readonly int ChunkHeight = 15;
+    public static readonly int WorldSizeInChunks = 50;
+    public static readonly int ViewDistanceInChunks = 8;
+    public static int WorldSizeInBlocks {
+        get { return WorldSizeInChunks * ChunkWidth; }
+    }
 
     public static readonly int TextureAtlasSizeInBlocks = 4;
     public static float NormalizedBlockTextureSize {
-
         get { return 1f / (float)TextureAtlasSizeInBlocks; }
-
     }
 
 	public static readonly Vector3[] voxelVerts = new Vector3[8] {
-
 		new Vector3(0.0f, 0.0f, 0.0f),
 		new Vector3(1.0f, 0.0f, 0.0f),
 		new Vector3(1.0f, 1.0f, 0.0f),
@@ -24,31 +26,24 @@ public static class VoxelData {
 		new Vector3(1.0f, 0.0f, 1.0f),
 		new Vector3(1.0f, 1.0f, 1.0f),
 		new Vector3(0.0f, 1.0f, 1.0f),
-
 	};
 
 	public static readonly Vector3[] faceChecks = new Vector3[6] {
-
 		new Vector3(0.0f, 0.0f, -1.0f),
 		new Vector3(0.0f, 0.0f, 1.0f),
 		new Vector3(0.0f, 1.0f, 0.0f),
 		new Vector3(0.0f, -1.0f, 0.0f),
 		new Vector3(-1.0f, 0.0f, 0.0f),
 		new Vector3(1.0f, 0.0f, 0.0f)
-
 	};
 
 	public static readonly int[,] voxelTris = new int[6,4] {
-
-        // Back, Front, Top, Bottom, Left, Right
-
-		// 0 1 2 2 1 3
-		{0, 3, 1, 2}, // Back Face
-		{5, 6, 4, 7}, // Front Face
-		{3, 7, 2, 6}, // Top Face
-		{1, 5, 0, 4}, // Bottom Face
-		{4, 7, 0, 3}, // Left Face
-		{1, 2, 5, 6} // Right Face
+		{0, 3, 1, 2}, // Back
+		{5, 6, 4, 7}, // Front
+		{3, 7, 2, 6}, // Top
+		{1, 5, 0, 4}, // Bottom
+		{4, 7, 0, 3}, // Left
+		{1, 2, 5, 6} // Right
 
 	};
 
@@ -60,4 +55,6 @@ public static class VoxelData {
 		new Vector2 (1.0f, 1.0f)
 
 	};
+
+
 }
--- Pixel_World/Assets/Scripts/Render/World.cs
+++ Pixel_World/Assets/Scripts/Render/World.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class World : MonoBehaviour {
+    public Transform player;
+    public Vector3 spawn;
+
 
     public Material material;
     public List<BlockType> blocktypes = new List<BlockType>();
+    
+    Chunk[,] chunks = new Chunk[VoxelData.WorldSizeInChunks, VoxelData.WorldSizeInChunks];
+    List<ChunkCoord> activeChunks = new List<ChunkCoord>();
+    ChunkCoord playerLastChunkCoord;
 
-    void Start(){
+    private void Start(){
         AddBlockTypes();
+        
+        GenerateWorld();
+        playerLastChunkCoord = GetChunkCoordFromVector3(player.transform.position);
+    }
+    
+    private void Update() {
+
+        if (!GetChunkCoordFromVector3(player.transform.position).Equals(playerLastChunkCoord))
+            CheckViewDistance();
+
     }
 
     void AddBlockTypes(){
+        AddBlockType(new BlockType("Air", false, 0, 0, 0, 0, 0, 0));
+        AddBlockType(new BlockType("Bedrock", true, 9, 9, 9, 9, 9, 9));
         AddBlockType(new BlockType("Stone", true, 0, 0, 0, 0, 0, 0));
         AddBlockType(new BlockType("Grass", true, 2, 2, 7, 1, 2, 2));
-        // AddBlockType(new BlockType("Dirt", true, 0, 0, 0, 0, 0, 0));
+        AddBlockType(new BlockType("Furnace", true, 13,12,15,15,13,13));
         // AddBlockType(new BlockType("Wood", true, 0, 0, 0, 0, 0, 0));
-        AddBlockType(new BlockType("Bedrock", true, 9, 9, 9, 9, 9, 9));
 
     }
     
@@ -24,6 +42,122 @@ public class World : MonoBehaviour {
     {
         blocktypes.Add(blockType);
         Debug.Log($"Block Type '{blockType.blockName}' registered.");
+    }
+    ChunkCoord GetChunkCoordFromVector3 (Vector3 pos) {
+
+        int x = Mathf.FloorToInt(pos.x / VoxelData.ChunkWidth);
+        int z = Mathf.FloorToInt(pos.z / VoxelData.ChunkWidth);
+        return new ChunkCoord(x, z);
+
+    }
+
+    private void GenerateWorld () {
+
+        for (int x = VoxelData.WorldSizeInChunks / 2 - VoxelData.ViewDistanceInChunks / 2; x < VoxelData.WorldSizeInChunks / 2 + VoxelData.ViewDistanceInChunks / 2; x++) {
+            for (int z = VoxelData.WorldSizeInChunks / 2 - VoxelData.ViewDistanceInChunks / 2; z < VoxelData.WorldSizeInChunks / 2 + VoxelData.ViewDistanceInChunks / 2; z++) {
+
+                CreateChunk(new ChunkCoord(x, z));
+
+            }
+        }
+
+        spawn = new Vector3(VoxelData.WorldSizeInBlocks / 2, VoxelData.ChunkHeight + 2, VoxelData.WorldSizeInBlocks / 2);
+        player.position = spawn;
+
+    }
+
+    private void CheckViewDistance () {
+
+        int chunkX = Mathf.FloorToInt(player.position.x / VoxelData.ChunkWidth);
+        int chunkZ = Mathf.FloorToInt(player.position.z / VoxelData.ChunkWidth);
+
+        List<ChunkCoord> previouslyActiveChunks = new List<ChunkCoord>(activeChunks);
+
+        for (int x = chunkX - VoxelData.ViewDistanceInChunks / 2; x < chunkX + VoxelData.ViewDistanceInChunks / 2; x++) {
+            for (int z = chunkZ - VoxelData.ViewDistanceInChunks / 2; z < chunkZ + VoxelData.ViewDistanceInChunks / 2; z++) {
+
+                // If the chunk is within the world bounds and it has not been created.
+                if (IsChunkInWorld(x, z)) {
+
+                    ChunkCoord thisChunk = new ChunkCoord(x, z);
+
+                    if (chunks[x, z] == null)
+                        CreateChunk(thisChunk);
+                    else if (!chunks[x, z].isActive) {
+                        chunks[x, z].isActive = true;
+                        activeChunks.Add(thisChunk);
+                    }
+                    // Check if this chunk was already in the active chunks list.
+                    for (int i = 0; i < previouslyActiveChunks.Count; i++) {
+
+                        //if (previouslyActiveChunks[i].Equals(new ChunkCoord(x, z)))
+                        if (previouslyActiveChunks[i].x == x && previouslyActiveChunks[i].z == z)
+                            previouslyActiveChunks.RemoveAt(i);
+
+                    }
+
+                }
+            }
+        }
+
+        foreach (ChunkCoord coord in previouslyActiveChunks)
+            chunks[coord.x, coord.z].isActive = false;
+
+    }
+
+    bool IsChunkInWorld(int x, int z) {
+
+        if (x > 0 && x < VoxelData.WorldSizeInChunks - 1 && z > 0 && z < VoxelData.WorldSizeInChunks - 1)
+            return true;
+        else
+            return false;
+
+    }
+
+    private void CreateChunk (ChunkCoord coord) {
+
+        chunks[coord.x, coord.z] = new Chunk(new ChunkCoord(coord.x, coord.z), this);
+        activeChunks.Add(new ChunkCoord(coord.x, coord.z));
+
+
+    }
+
+    public byte GetVoxel (Vector3 pos) {
+
+        if (pos.x < 0 || pos.x > VoxelData.WorldSizeInBlocks - 1 || pos.y < 0 || pos.y > VoxelData.ChunkHeight - 1 || pos.z < 0 || pos.z > VoxelData.WorldSizeInBlocks - 1)
+            return 0;
+        if (pos.y < 1)
+            return 1;
+        else if (pos.y == VoxelData.ChunkHeight - 1)
+            return 3;
+        else
+            return 2;
+
+    }
+
+}
+
+public class ChunkCoord {
+
+    public int x;
+    public int z;
+
+    public ChunkCoord (int _x, int _z) {
+
+        x = _x;
+        z = _z;
+
+    }
+
+    public bool Equals(ChunkCoord other) {
+
+        if (other == null)
+            return false;
+        else if (other.x == x && other.z == z)
+            return true;
+        else
+            return false;
+
     }
 
 }
@@ -73,6 +207,8 @@ public class BlockType {
             default:
                 Debug.Log("Error in GetTextureID; invalid face index");
                 return 0;
+
+
         }
 
     }
