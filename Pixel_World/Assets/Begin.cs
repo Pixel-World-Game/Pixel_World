f0c69d06b49f8db7c1ecf59955c7e78c6a6bae3e
Hui0427 <clairehui0427@gmail.com>
2025-01-06 20:15:38 +0000

    image and audio assets

---
+++ Pixel_World/Assets/Begin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Begin : MonoBehaviour {
+
+    public GameObject gameObject1;
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Next(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+  
+}
---
+++ Pixel_World/Assets/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Move : MonoBehaviour {
+
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+      
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(this.transform.forward * Time.deltaTime*3,Space.World);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            if (other.tag == "Monster")
+            {
+                other.GetComponent<Monster>().HP -= 20;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
